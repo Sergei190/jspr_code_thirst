e140ae234dd16b66fa78e7206c32783b79d3af4b
Sergei190 <kudrasovcergej@gmail>
2023-07-11 14:52:29 +0500

    the program has been improved

--- src/main/java/server/Request.java
+++ src/main/java/server/Request.java
@@ -1,10 +1,14 @@
 package server;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Request {
     private String path;
-    private Map<String, String> queryParams;
+    private Map<String, String> queryParams;    private Map<String, String> postParams;
+
 
     public Request(String path, Map<String, String> queryParams) {
         this.path = path;
@@ -21,5 +25,17 @@ public class Request {
 
     public String getQueryParam(String name) {
         return queryParams.get(name);
+    }          public void parsePostParameters(String body) {
+        postParams = new HashMap<>();
+
+        String[] params = body.split("&");
+        for (String param : params) {
+            String[] keyValue = param.split("=");
+            if (keyValue.length == 2) {
+                String key = URLDecoder.decode(keyValue[0], StandardCharsets.UTF_8);
+                String value = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
+                postParams.put(key, value);
+            }
+        }
     }
 }
